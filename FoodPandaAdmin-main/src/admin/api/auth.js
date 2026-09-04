6e65b63750c4c54f162bb276db885e1e5c50a00a
rohitsen0604-source <rohitsen0604@gmail.com>
2026-09-04 17:05:22 +0530

    feat: implement useAdminAuth and useChangePassword hooks for authentication and password management

--- FoodPandaAdmin-main/src/admin/api/auth.js
+++ FoodPandaAdmin-main/src/admin/api/auth.js
@@ -1,4 +1,5 @@
-import { useState,useNavigate } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { API_BASE_URL } from '../../utils/utils';
