492611472367ebc419700a381acc7c8266ed9ecc
rohitsen0604-source <rohitsen0604@gmail.com>
2026-09-04 18:31:12 +0530

    fixing login issue

--- FoodPandaAdmin-main/src/admin/dashboard/components/Logout.jsx
+++ FoodPandaAdmin-main/src/admin/dashboard/components/Logout.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+import { API_BASE_URL } from '../../../utils/utils';
 
 export default function LogoutButton() {
   const navigate = useNavigate();
